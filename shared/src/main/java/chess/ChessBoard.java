392690cb48e0cb592ab1fa76b6b673ed95253bd3
Doman Briggs <dbbriggs@byu.edu>
2025-01-21 09:59:44 -0700

    teamStuff

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,10 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] board;
+
     public ChessBoard() {
-        
+        resetBoard();
     }
 
     /**
@@ -38,6 +40,7 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+
+
     }
 }
